96694edb3cebb6f13f85e2a71f4343957c931e71
alexandre-dev29 <axel.business29@gmail.com>
2022-09-07 21:22:47 +0200

    adding final configuration for deploying on windows vps with IISNode

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,6 +1,7 @@
-module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("MY_HEROKU_URL"),
+export default ({ env }) => ({
+  host: "localhost",
+  port: `${process.env.PORT || 8081}`,
+  proxy: { enabled: false },
   app: {
     keys: env.array("APP_KEYS"),
   },
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: "localhost",
+  port: `${process.env.PORT || 8081}`,
+  proxy: { enabled: false },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
